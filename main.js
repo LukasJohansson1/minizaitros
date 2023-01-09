f8640bfaf498e613a06f3fbc5c15f08234763d05
Lukas Johansson <113909170+LukasJohansson1@users.noreply.github.com>
2023-01-09 21:44:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
-const menu_btn = document.querySelector(".hamburger");
-const mobile_menu = document.querySelector(".mobile-nav");
+const btn = document.querySelector(".hamburger");
+const menu = document.querySelector(".mobile-nav");
 
-menu_btn.addEventListener("click", function() {
-    menu_btn.classList.toggle("is-active");
-    mobile_menu.classList.toggle("is-active");
+btn.addEventListener("click", function() {
+    btn.classList.toggle("is-active");
+    menu.classList.toggle("is-active");
 });
+
+document.querySelectorAll(".link").forEach(n => n.addEventListener("click", () => {
+    btn.classList.remove("is-active");
+    menu.classList.remove("is-active")
+}))
