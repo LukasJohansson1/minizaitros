b0182771af6bad77ae70ed365954b70072398776
Lukas <lukas.johansson@skola.taby.se>
2023-01-12 14:48:41 +0100

    hej

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const btn = document.querySelector(".hamburger");
 const menu = document.querySelector(".mobile-nav");
 
 btn.addEventListener("click", function() {
-    btn.classList.toggle("is-active");
-    menu.classList.toggle("is-active");
+    btn.classList.toggle("active");
+    menu.classList.toggle("active");
 });
 
 document.querySelectorAll(".link").forEach(n => n.addEventListener("click", () => {
     
-    btn.classList.remove("is-active");
-    menu.classList.remove("is-active");
+    btn.classList.remove("active");
+    menu.classList.remove("active");
 }))
